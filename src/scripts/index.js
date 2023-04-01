22631e1ffc6b7cb09dd07dde8e27713feb68e6ea
Daniel Ugarov <tyradire@yandex.ru>
2023-04-01 21:46:06 +0300

    chore: add fetch description

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Скрипт загружен')
+
+  const dataUrl = 'https://baconipsum.com/api/?type=lucky';
+
+  const heroInput = document.querySelector('.form__input');
   const flags = document.querySelectorAll('.animate-side__flag');
-  console.log(flags);
+  const description = document.querySelector('.user-side__description');
+
   setTimeout(() => {
     flags.forEach(elem => {
       elem.style.animation = 'testAnimation 20s infinite linear'
       elem.style.opacity = 1
     })
   }, 2600);
+
+  function testFunc(e) {
+    const regex = /[!@#$%^&*()]/;
+    if (e.key.match(regex)) e.preventDefault();
+  }
+  heroInput.addEventListener('keypress', (e) => testFunc(e))
+
+  fetch(dataUrl)
+    .then(res => res.json())
+    .then(res => description.textContent = res[0])
 });
