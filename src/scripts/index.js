05e7f626203b266f10c9e594019ae43f7438adbe
Daniel Ugarov <tyradire@yandex.ru>
2023-04-01 16:09:08 +0300

    chore: add flags animation draft

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Скрипт загружен')
+  const flags = document.querySelectorAll('.animate-side__flag');
+  console.log(flags);
+  // setTimeout(() => {
+  //   flags.forEach(elem => elem.classList.add('animate-side__flag_active'))
+  // }, 4600);
 });
