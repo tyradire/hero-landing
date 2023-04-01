615c66d142c6fc84614a8d6039314a747b1d56c7
Daniel Ugarov <tyradire@yandex.ru>
2023-04-01 16:53:06 +0300

    chore: add flags animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Скрипт загружен')
   const flags = document.querySelectorAll('.animate-side__flag');
   console.log(flags);
-  // setTimeout(() => {
-  //   flags.forEach(elem => elem.classList.add('animate-side__flag_active'))
-  // }, 4600);
+  setTimeout(() => {
+    flags.forEach(elem => {
+      elem.style.animation = 'testAnimation 20s infinite linear'
+      elem.style.opacity = 1
+    })
+  }, 2600);
 });
