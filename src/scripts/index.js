a5bd7c219d9c55663e20cdb8a4dbf8afc3066e84
Daniel Ugarov <tyradire@yandex.ru>
2023-04-02 15:22:48 +0300

    feat: add mobile menu button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const heroInput = document.querySelector('.form__input');
   const flags = document.querySelectorAll('.animate-side__flag');
   const description = document.querySelector('.user-side__description');
+  const headerMenu = document.querySelector('.header__menu');
+  const burgerMenuButton = document.querySelector('.header__button-menu');
+  const checkboxMenuButton = document.querySelector('.header__checkbox-menu');
 
   setTimeout(() => {
     flags.forEach(elem => {
@@ -13,13 +16,51 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }, 2600);
 
+  let windowSize = 0;
+  let screenType = 'desktop';
+
+  const setWindowSize = () => {
+    windowSize = window.innerWidth
+    if (windowSize > 992) {
+      screenType = 'desktop';
+      headerMenu.style.display = 'block';
+      burgerMenuButton.style.display = 'none';
+    }
+    else if (windowSize < 600) {
+      screenType = 'mobile';
+      burgerMenuButton.style.display = 'block';
+    } else if (windowSize < 769) {
+      screenType = 'tablet';
+      burgerMenuButton.style.display = 'block';
+    }
+    else {
+      screenType = 'laptop';
+      headerMenu.style.display = 'block';
+      burgerMenuButton.style.display = 'none';
+    }
+    console.log(windowSize, screenType)
+  }
+
   function testFunc(e) {
     const regex = /[!@#$%^&*()]/;
     if (e.key.match(regex)) e.preventDefault();
   }
   heroInput.addEventListener('keypress', (e) => testFunc(e))
 
-  fetch(dataUrl)
+  window.addEventListener("resize", setWindowSize);
+
+  const openMobileMenu = () => {
+    headerMenu.classList.toggle('header__menu_opened');
+  }
+
+  checkboxMenuButton.addEventListener('change', openMobileMenu)
+  
+  const initialization = () => {
+    setWindowSize();
+    fetch(dataUrl)
     .then(res => res.json())
-    .then(res => description.textContent = res[0])
+    .then(res => description.textContent = res[0]);
+  }
+
+  initialization();
 });
